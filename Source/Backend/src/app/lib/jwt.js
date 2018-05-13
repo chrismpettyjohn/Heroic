f0e17d8705db958bc8d156e8ee6bc536960fba99
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-05-12 22:09:20 -0500

    Fixed JWT signing issue and put it in config.json

--- Source/Backend/src/app/lib/jwt.js
+++ Source/Backend/src/app/lib/jwt.js
@@ -3,24 +3,23 @@ import Path from 'path'
 import Token from 'jsonwebtoken'
 export default class Library {
 
-  static async sign (session) {
+  static async sign(session) {
+    const config = JSON.parse(File.readFileSync('./config.json', 'utf8')).jwt
     try {
-      return Token.sign(session, File.readFileSync(Path.resolve(__dirname, '..', 'config', 'https', 'key.pem')), {
-          expiresIn: '24h',
-          algorithm: 'HS256'
-        })
-    }
-    catch (error) {
+      return Token.sign(session, config, {
+        expiresIn: '24h',
+        algorithm: 'HS256'
+      })
+    } catch (error) {
       throw new Error(error)
     }
   }
 
-  static async validate (session) {
+  static async validate(session) {
     if (session) {
       try {
-        return Token.verify(session, File.readFileSync(Path.resolve(__dirname, '..', 'config', 'https', 'key.pem')))
-      }
-      catch (error) {
+        return Token.verify(session, config)
+      } catch (error) {
         throw new Error(error)
       }
     } else {
