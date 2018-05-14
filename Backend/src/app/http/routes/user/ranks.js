a85f9305ac255661e32787ebc4ba254da69a649a
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-05-14 16:13:57 -0500

    Fix issue with store purchasing

--- Backend/src/app/http/routes/user/ranks.js
+++ Backend/src/app/http/routes/user/ranks.js
@@ -1,7 +1,7 @@
 import Controller from '../../controller/user/ranks'
 export default class Routes {
 
-  constructor (http) {
+  constructor(http) {
     http.get('/api/ranks', Controller.read)
     http.get('/api/ranks/:id', Controller.read)
   }
