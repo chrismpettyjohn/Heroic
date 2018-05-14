afa33f64434dbf6fc294bee8b1757832bf62161b
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-05-14 06:45:35 -0500

    Change boot process to add npm i

--- Backend/src/boot.js
+++ Backend/src/boot.js
@@ -1,6 +1,13 @@
+// Clear Terminal
 process.stdout.write('\x1B[2J')
+console.log('>Running system checks')
 
-//Config
+// Dependencies
+const file = require('fs')
+const child = require('child_process').execSync
+
+// Local Environment
+// Configure Heroic Environment
 let path = ''
 if (process.pkg) {
   path = process.argv[0]
@@ -12,8 +19,25 @@ if (process.pkg) {
   path = process.cwd()
 }
 
-global.config = require(`${path}/config.json`)
+// Run()
+const run = function() {
+  global.config = require(`${path}/config.json`)
+  // Initiate Heroic
+  require('./app/heroic').default.init()
+}
 
+// node_modules check
+if (file.existsSync('./node_modules')) {
+  run()
+} else {
+  // Notify User
+  console.log('>Installing Dependencies')
+  console.log('>This may take a minute')
 
-// Launch
-require('./app/heroic').default.init()
+  // Run Bash Commands
+
+  child('npm install', ((error, stdout, stderr) => {
+    run()
+  }))
+
+}
