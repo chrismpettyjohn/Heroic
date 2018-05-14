43d57d1abe28e6a81b67e2c7f1b0aec03031719f
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-05-14 17:34:16 -0500

    Prepares for Commander intergration (NOT USABLE YET)

--- Backend/src/app/heroic.js
+++ Backend/src/app/heroic.js
@@ -1,9 +1,13 @@
+import Commander from '/Users/chris/projects/Heroic-Module-Commander/src/index'
 import Website from './http/server'
 export default class Heroic {
 
   static async init() {
     try {
+      // Website Service
       await Website.init()
+      // Emulator Service
+      await Commander.init(global.config)
       console.log(`>Heroic ${global.version} has started on ${global.config.http.port}`)
     } catch (error) {
       console.log(`${error}\nHeroic will now shutdown.`)
