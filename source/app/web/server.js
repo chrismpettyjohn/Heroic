dbf91cf9eb7bc06132a28418398d4582613cb3e6
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-05-19 10:58:08 -0500

    Undo changes

--- source/app/web/server.js
+++ source/app/web/server.js
@@ -1,9 +1,8 @@
-import Glob from 'glob'
-import Fastify from 'fastify'
-import Heroic from '~/app/heroic'
-import formBody from 'fastify-formbody'
+import Glob from "glob";
+import Fastify from "fastify";
+import Heroic from "~/app/heroic";
+import formBody from "fastify-formbody";
 export default class HTTP {
-
   static server = {};
   static middleware = [];
 
@@ -16,89 +15,99 @@ export default class HTTP {
         await HTTP.loadRouting();
         await HTTP.loadStaticRouting();
         await HTTP.listen();
-        resolve(`HTTP Server is listening on port ${Heroic.Config.http.port}`)
+        resolve(`HTTP Server is listening on port ${Heroic.Config.http.port}`);
       } catch (error) {
-        reject(`HTTP Server - ${error}`)
+        reject(`HTTP Server - ${error}`);
       }
-    })
+    });
   }
 
   static prepare() {
     return new Promise((resolve, reject) => {
-      HTTP.server = new Fastify({http2: Heroic.Config.http.http2});
-      resolve()
-    })
+      HTTP.server = new Fastify({ http2: Heroic.Config.http.http2 });
+      resolve();
+    });
   }
 
   static configure() {
     return new Promise((resolve, reject) => {
       HTTP.server.register(formBody);
-      resolve()
-    })
+      resolve();
+    });
   }
 
   static loadMiddleware() {
     return new Promise((resolve, reject) => {
-      Glob(`${__dirname}/middleware/**/*.js`, ((errors, middleware) => {
+      Glob(`${__dirname}/middleware/**/*.js`, (errors, middleware) => {
         if (!errors) {
           middleware.forEach(middle => {
             let component = require(middle).default;
-            HTTP.middleware[component.name] = component.handle
+            HTTP.middleware[component.name] = component.handle;
           });
-          resolve()
+          resolve();
         } else {
-          reject(errors)
+          reject(errors);
         }
-      }))
-    })
+      });
+    });
   }
 
   static loadRouting() {
     return new Promise((resolve, reject) => {
-      Glob(`${__dirname}/routes/**/*.json`, ((errors, routes) => {
+      Glob(`${__dirname}/routes/**/*.json`, (errors, routes) => {
         if (!errors) {
           routes.forEach(route => {
             route = require(route);
-            let controller = require(`${__dirname}/controllers/${route.controller}`).default;
+            let controller = require(`${__dirname}/controllers/${
+              route.controller
+            }`).default;
             route.routes.forEach(data => {
               let link = `/api/${route.prefix}/${data.link}`;
               // Adjust for empty GET
               if (data.link.length == 0) {
-                link = link.slice(0, -1)
+                link = link.slice(0, -1);
               }
               if (data.middleware) {
-                HTTP.server[data.method.toLowerCase()](link, {
-                  beforeHandler: HTTP.middleware[data.middleware]
-                }, controller[data.controller])
+                HTTP.server[data.method.toLowerCase()](
+                  link,
+                  {
+                    beforeHandler: HTTP.middleware[data.middleware]
+                  },
+                  controller[data.controller]
+                );
               } else {
-                HTTP.server[data.method.toLowerCase()](link, controller[data.controller])
+                HTTP.server[data.method.toLowerCase()](
+                  link,
+                  controller[data.controller]
+                );
               }
-            })
+            });
           });
-          resolve()
+          resolve();
         } else {
-          reject(errors)
+          reject(errors);
         }
-      }))
-    })
+      });
+    });
   }
 
   static loadStaticRouting() {
     return new Promise((resolve, reject) => {
-      resolve()
-    })
+      resolve();
+    });
   }
 
   static listen() {
     return new Promise((resolve, reject) => {
-      HTTP.server.listen(Heroic.Config.http.port, (error => {
+      HTTP.server.listen(Heroic.Config.http.port, error => {
         if (error) {
-          reject(`HTTP server cannot listen on port ${Heroic.Config.http.port}`)
+          reject(
+            `HTTP server cannot listen on port ${Heroic.Config.http.port}`
+          );
         } else {
-          resolve()
+          resolve();
         }
-      }))
-    })
+      });
+    });
   }
-
 }
