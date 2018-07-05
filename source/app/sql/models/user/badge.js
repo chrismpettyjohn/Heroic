e04526a0d7dcd6600012c4de08b3f1b743e1ad08
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-07-05 00:10:50 -0500

    Empty Repository (Prep)
    Prepatory commit for Arctic Fox integration

--- source/app/sql/models/user/badge.js
+++
@@ -1,5 +0,0 @@
-import Model from '~/app/sql/model'
-export default class Badges extends Model {
-  static tableName = 'users_badges';
-  static visible = ['id', 'user_id', 'slot_id', 'badge_code'];
-}
