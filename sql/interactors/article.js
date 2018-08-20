11f6c881864ebc1e492623948f1565101a80da41
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-08-20 04:16:25 -0500

    Article API - Add latest articles
    - Adds the latest 10 articles by querying `latest` instead of an id

--- sql/interactors/article.js
+++ sql/interactors/article.js
@@ -3,7 +3,12 @@ export default class Interactor {
   // Fetch article
   static async read (id, relations = '') {
     try {
-      let article = await Model.query().eager(`[${relations}]`).findById(id)
+      let article = {}
+      if (id!=='latest') {
+        article = await Model.query().eager(`[${relations}]`).findById(id)
+      } else {
+        article = await Model.query().eager(`[${relations}]`).orderBy('id', 'DESC').limit(10).select()
+      }
       if (article) {
         return Promise.resolve(article)
       } else {
