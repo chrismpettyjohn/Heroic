f8cbad6736ebd23b44b5a31f00730af51bf0c573
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-08-20 06:02:38 -0500

    Camera API - Add user relation

--- api/sql/models/camera.js
+++ api/sql/models/camera.js
@@ -6,12 +6,32 @@ export default class Model extends Base {
 
   static get visible () {
     return [
+      // Columns
       'id',
       'user_id',
       'room_id',
       'timestamp',
-      'url'
+      'url',
+      // Relationships
+      'author'
     ]
   }
-  
+
+  static get relationMappings () {
+    // Dependencies
+    const User = require('./user').default
+    // Relations
+    return {
+      // Author (users)
+      author:{
+        relation: Base.HasOneRelation,
+        modelClass: User,
+        join: {
+          from: 'camera_web.user_id',
+          to: 'users.id'
+        }
+      }
+    }
+  }
+
 }
