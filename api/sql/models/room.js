12fbdec5422a6d3d32d35600e62b1721b6a83fb2
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-08-21 14:27:07 -0500

    Rooms API - Add owner relationship
    - Adds `owner`relationship to SQL model

--- api/sql/models/room.js
+++ api/sql/models/room.js
@@ -6,6 +6,7 @@ export default class Model extends Base {
 
   static get visible () {
     return [
+      // Columns
       'id',
       'owner_id',
       'owner_name',
@@ -17,7 +18,27 @@ export default class Model extends Base {
       'guild_id',
       'tags',
       'is_public',
-      'is_staff_picked'
+      'is_staff_picked',
+      // Relationships
+      'owner'
     ]
   }
+
+  static get relationMappings () {
+    // Dependencies
+    const User = require('./user').default
+    // Relations
+    return {
+      // Owner (users)
+      owner: {
+        relation: Base.HasOneRelation,
+        modelClass: User,
+        join: {
+          from: 'rooms.owner_id',
+          to: 'users.id'
+        }
+      }
+    }
+  }
+
 }
