8be621c206578f14b0aa169b5c48d41353fdace6
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-09-04 20:29:43 -0500

    Session API - Fix session info bug

--- api/web/controllers/session.js
+++ api/web/controllers/session.js
@@ -5,7 +5,7 @@ import Interactor from '@/sql/interactors/user'
 export default class Controller {
   static async read (request, reply) {
     try {
-      let user = await Interactor.read(request.session.id)
+      let user = await Interactor.read(request.session.username)
       reply.code(201).send(user)
     } catch (e) {
       reply.code(400).send(e)
