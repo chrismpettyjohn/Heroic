be543ecef0ce6082411ce458c9ed4d581a6e9f43
Chris Pettyjohn <cpettyjohn@drivestl.com>
2019-05-05 17:01:01 -0500

    WIP

--- web/src/components/core/router.tsx
+++ web/src/components/core/router.tsx
@@ -1,9 +1,48 @@
-import React, {Component} from 'react'
+import Routes from 'app/routes'
+import React, {PureComponent} from 'react'
+import {Route} from 'react-router-dom'
 
-export default class extends Component {
+interface IState {
+	ready: boolean,
+	routes: Array<IRoute>
+}
+
+interface IRoute {
+	to: string,
+	component: any
+}
+
+export default class extends PureComponent<{}> {
+
+	state: IState = {
+		ready: false,
+		routes: []
+	}
+
+	componentWillMount () {
+		this.prepare(Routes)
+	}
+
+	prepare = routes => {
+
+		const final: Array<IRoute> = routes.map(parent => this.format(parent))
+
+		this.setState({
+			ready: true,
+			routes: final
+		})
+
+	}
+
+	format = parent => {
+		return {}
+	}
 
 	render () {
-		return <p>Router</p>
+		const {ready,routes} = this.state
+		return !ready
+			? null
+			: routes.map( ({ to, component }) => <Route to={to} component={component}/>)
 	}
 
 }
