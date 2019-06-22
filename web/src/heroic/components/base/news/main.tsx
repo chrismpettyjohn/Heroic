c650e0aa4ede7f2c48030297e26efa100e6e0c90
Chris Pettyjohn <cpettyjohn@drivestl.com>
2019-06-22 12:08:09 -0500

    Small design change

--- web/src/heroic/components/base/news/main.tsx
+++ web/src/heroic/components/base/news/main.tsx
@@ -5,7 +5,7 @@ import Article from 'heroic/app/interface/data/article'
 export default ({ campaign, description, title, timestamp, image }: Article) => (
 	<Column size={12}>
 		<div>
-			<img src={`https://images.habbo.com/web_images/habbo-web-articles/${image}.png`}/>
+			<img src={`https://images.habbo.com/web_images/habbo-web-articles/${image}.png`} style={{ boxShadow: '3px 3px rgba(0,0,0,.3)' }}/>
 		</div>
 	</Column>
 )
