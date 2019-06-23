d102e26e23d33ed9e029a2d0e516f1c4f0286fbe
Chris Pettyjohn <cpettyjohn@drivestl.com>
2019-06-23 17:21:55 -0500

    Remove news shit

--- web/src/heroic/pages/user/home/me.tsx
+++ web/src/heroic/pages/user/home/me.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import News from 'heroic/components/base/news'
 import Card from 'heroic/components/base/card'
 import Alert from 'heroic/components/base/alert'
 import {Column, Row} from 'heroic/components/layout/grid'
@@ -23,18 +22,11 @@ export default () => (
 		</ContainerHeader>
 		<ContainerContent>
 			<Row>
-				<Column size={5}>
+				<Column size={6}>
 					<h4>Hotel Announcement</h4>
 					<Alert color="danger" header="Help us with development">
 						<p>We now use React, fuck all of ya'll.</p>
 					</Alert>
-					<News/>
-				</Column>
-				<Column size={2}/>
-				<Column size={4}>
-					<Card header="Heroic Rules" subheader="RevCMS suxx">
-						<p>It's true</p>
-					</Card>
 				</Column>
 			</Row>
 		</ContainerContent>
