f4de77aaf6c48b1c6dad9eb5121da82245355c4a
Chris Pettyjohn <cpettyjohn@drivestl.com>
2019-06-25 20:36:04 -0500

    Add cheeky design for settings page

--- web/src/heroic/pages/user/home/settings.tsx
+++ web/src/heroic/pages/user/home/settings.tsx
@@ -1,11 +1,30 @@
 import React, {PureComponent} from 'react'
 import Redux from 'heroic/app/interface/redux'
 import Stateful from 'heroic/app/redux/stateful'
+import {Column, Row} from 'heroic/components/layout/grid'
+import SubHeader from 'heroic/components/layout/sub-header'
+import Container, {ContainerContent, ContainerHeader} from 'heroic/components/layout/container'
 
 class Settings extends PureComponent<Redux> {
 
 	render () {
-		return <div/>
+		return (
+			<Container>
+				<ContainerHeader>
+					<SubHeader>
+						<h2>Settings</h2>
+						<p>Manage your credentials and privacy settings</p>
+					</SubHeader>
+				</ContainerHeader>
+				<ContainerContent>
+					<Row>
+						<Column size={6}>
+							<h4>Coming soon</h4>
+						</Column>
+					</Row>
+				</ContainerContent>
+			</Container>
+		)
 	}
 
 }
