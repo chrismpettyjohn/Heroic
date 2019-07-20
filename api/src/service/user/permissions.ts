f008d141603664e6c92990d211b4cfb152cad140
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2019-07-20 10:51:31 -0500

    Implemented query helper for searching permissions

--- api/src/service/user/permissions.ts
+++ api/src/service/user/permissions.ts
@@ -1,16 +1,35 @@
 import {Permissions} from "db/entity/permissions";
 import {getRepository, Repository, Like} from "typeorm";
 
+const BASE_RELATIONS: string[] = ['users']
+
 const PermissionsRepository: Repository<Permissions> = getRepository(Permissions)
 
 export default {
 
 	create: async (permission: Permissions): Promise<Permissions> => PermissionsRepository.save(permission),
 
-	list: async (): Promise<Permissions[]> => PermissionsRepository.find(),
+	list: async (): Promise<Permissions[]> => PermissionsRepository.find({ relations: BASE_RELATIONS }),
 
-	read: async (id: number): Promise<Permissions> => PermissionsRepository.findOne(id),
+	read: async (id: number): Promise<Permissions> => PermissionsRepository.findOne({
+		where: {
+			id: id
+		},
+		relations: ['users']
+	}),
 
-	search: async (name: string): Promise<Permissions[]> => PermissionsRepository.find({ rank_name: Like(`%${name}%`) })
+	query: async (key: string, value: string): Promise<Permissions[]> => PermissionsRepository.find({
+		where: {
+			[key]: value
+		},
+		relations: BASE_RELATIONS
+	}),
+
+	search: async (name: string): Promise<Permissions[]> => PermissionsRepository.find({
+		where: {
+			rank_name: Like(`%${name}%`)
+		},
+		relations: BASE_RELATIONS
+	})
 
 }
