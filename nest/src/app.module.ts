65c47d159df6450dcd8fa5d260022261b83e24ad
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2019-08-30 20:50:01 -0500

    Remove useless code now, as env is loaded in package.json

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -7,14 +7,7 @@ import { SecurityModule} from './security/module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST || '127.0.0.1',
-      port: parseInt(process.env.DATABASE_PORT) || 3306,
-      username: process.env.DATABASE_USER || 'root',
-      password: process.env.DATABASE_PASS || '',
-      database: process.env.DATABASE_NAME || ''
-    }),
+    TypeOrmModule.forRoot(),
     AuthModule,
     UserModule,
     ArticleModule,
