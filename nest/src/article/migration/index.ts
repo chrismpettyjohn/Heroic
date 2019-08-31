227160d3f0f07fbc5e0c0ed74f9cc0abfa7b101a
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2019-08-30 21:14:54 -0500

    Add foreign key to heroic_articles for category SQL

--- nest/src/article/migration/index.ts
+++ nest/src/article/migration/index.ts
@@ -33,6 +33,10 @@ export class Initial1567216000284 implements MigrationInterface {
       "  CONSTRAINT `heroic_articles.user_id` FOREIGN KEY (`user_id`) REFERENCES `morningstar`.`users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE\n" +
       ");"
     );
+    // Create foreign keys
+    await queryRunner.query('ALTER TABLE `heroic_articles` \n' +
+      'ADD CONSTRAINT `heroic_articles.category_id` FOREIGN KEY (`category_id`) REFERENCES `morningstar`.`heroic_articles_categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;'
+    );
   }
 
   async down(queryRunner: QueryRunner): Promise<any> {
